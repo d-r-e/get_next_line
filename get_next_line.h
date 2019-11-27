3ff56cb5dd69b13c6300555644531aa852ff39bd
David Rodríguez <9976038+d-r-e@users.noreply.github.com>
2019-11-27 14:19:27 +0100

    final version

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: darodrig <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/26 09:59:23 by darodrig          #+#    #+#             */
-/*   Updated: 2019/11/26 14:31:32 by darodrig         ###   ########.fr       */
+/*   Updated: 2019/11/27 14:14:42 by darodrig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,11 @@
 #  define BUFFER_SIZE 32
 # endif
 
-int		get_line(int fd, char **s, char **line);
-int		get_next_line(int fd, char **line);
-size_t	ft_strlen(const char *s);
-char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strdup(const char *s1);
-char	*ft_substr(char const *s2, unsigned int start, size_t len);
-char	*ft_strchr(const char *s, int c);
+int			get_next_line(int fd, char **line);
+size_t		ft_strlen(const char *s);
+char		*ft_strjoin(char const *s1, char const *s2);
+char		*ft_strdup(const char *s1);
+char		*ft_substr(char const *s2, unsigned int start, size_t len);
+char		*ft_strchr(const char *s, int c);
 
 #endif
